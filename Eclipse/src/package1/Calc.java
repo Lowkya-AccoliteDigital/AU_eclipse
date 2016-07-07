a0344445fab1e04b54b3b05fddbb05ecbce104c2
Lowkya Vuppu <Lowkya Vuppu@LOVU-BLR-01>
2016-07-07 11:30:03 +0530

    first commit

---
+++ Eclipse/src/package1/Calc.java
@@ -0,0 +1,66 @@
+package package1;
+
+import java.util.Scanner;
+
+public class Calc implements Calculator {
+
+	@Override
+	public int add(int number1, int number2) {
+		// TODO Auto-generated method stub
+		return number1 + number2;
+	}
+
+	@Override
+	public int subract(int number1, int number2) {
+		// TODO Auto-generated method stub
+		return number1 - number2;
+	}
+
+	@Override
+	public int multiply(int number1, int number2) {
+		// TODO Auto-generated method stub
+		return number1 * number2;
+	}
+
+	@Override
+	public int divide(int number1, int number2) {
+		// TODO Auto-generated method stub
+		return number1 / number2;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calc =new Calc();
+		Scanner scan =new Scanner(System.in);
+		
+		int number1= scan.nextInt();
+		int number2= scan.nextInt();
+		String operator =scan.next();
+		
+		try{
+			
+			switch(operator)
+			{
+			case "+":
+				System.out.println( calc.add(number1, number2));
+				break;
+			case "-":
+				System.out.println( calc.subract(number1, number2));
+				break;
+			case "*":
+				System.out.println( calc.multiply(number1, number2));
+				break;
+			case "/":
+				System.out.println( calc.divide(number1, number2));
+				break;
+			default:
+				System.out.println("this not the operator expected");
+			}
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("exception raised");
+		}
+	}
+
+}
---
+++ Eclipse/src/package1/Calculator.java
@@ -0,0 +1,8 @@
+package package1;
+
+public interface Calculator {
+	public int add(int number1, int number2);
+	public int subract(int number1, int number2);
+	public int multiply(int number1, int number2);
+	public int divide(int number1, int number2);
+}
